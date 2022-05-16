a7d6a1fbcd073b15934aad009f032da40d1fb9e5
jocoly <62028785+jocoly@users.noreply.github.com>
2022-05-15 23:14:39 -0400

    program grader entrypoint

--- src/SubmissionGrader/GradeAssignments.go
+++ src/SubmissionGrader/GradeAssignments.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"fmt"
-	"github.com/go-git/go-git/v5"
-	//"github.com/go-git/go-git/storage/memory"
-	memory "github.com/go-git/go-git/v5/storage/memory"
 	"os"
 	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
 	// "runtime"
 )
-
-func GetEnvVar(envName string) string {
-	if envName != "" {
-		ret := os.Getenv("envName")
-		return ret
-	}
-	fmt.Printf(envName + "environment variable was not found.")
-	return "error"
-}
 
 var (
 	//secret token
@@ -37,49 +28,66 @@ var (
 	semesterID                = GetEnvVar("SEMESTER_ID")
 	studentUserName           = GetEnvVar("SUDENT_USERNAME")
 
+	config        = "IUS-CS/AutoGraderConfig"
+	containerName = repoName + "-Container"
 	// copyToPath = "/tmp/AutoGrader/$CourseID-$SemesterID/$AssignmentName/$StudentUserName"
-	copyToPath = "/tmp/AutoGrader/" + courseID + "-" + semesterID + "/" + assignmentName + "/" + studentUserName
+	tempPath = "/tmp/AutoGrader/" + courseID + "-" + semesterID + "/" + assignmentName + "/" + studentUserName
 )
 
-func main() {
+func GetEnvVar(envName string) string {
+	if envName != "" {
+		ret := os.Getenv("envName")
+		return ret
+	}
+	fmt.Printf(envName + "environment variable was not found.")
+	return "error"
+}
 
-	// done: start with environment variables
-	// done: switch statement
-	// TODO: one language at a time
-	// TODO: java>c++>duplicate
-	// TODO: sigterm handling on grader level
-
+func languageSwitch(ctx context.Context) error {
+	var err error
 	switch language {
 	case "java":
 		{
-			cmd := exec.Command("git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", common.GithubAPIKey["token"], *repo.FullName))
-			cmd.Dir = "/opt/gradle"
-			err := cmd.Run()
-			return err
-
+			//clone repo
+			err = cloneRepo(githubToken, repoName)
+			if err != nil {
+				return err
+			}
 			if teacherUnitTestsEnabled == "TRUE" {
-
-				cmd := exec.Command("cp", "--recursive", oldDir, newDir)
-				cmd.Run()
-				//git.Clone(memory.NewStorage(), nil, &git.CloneOptions{URL: "https://" + githubToken + "@github.com/IUS-CS/AutoGraderConfig.git"})
-				//common.RunDockerCommand(cli, containerName,
-				//	[]string{"git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", common.GithubAPIKey["token"], "IUS-CS/AutoGraderConfig")},
-				//	"/tmp", verbose)
+				err = cloneConfigRepo(githubToken)
+				if err != nil {
+					return err
+				}
+				//copy tests to folder
+				err = copyTestsToFolder(repoName)
+				if err != nil {
+					return err
+				}
 			}
 			if gradeDocsEnabled == "TRUE" {
 				fmt.Println("Grading java")
+				err = gradeDocs(repoName)
+				if err != nil {
+					return err
+				}
+			}
+			if studentTestsEnabled == "TRUE" || teacherUnitTestsEnabled == "TRUE" {
+				//err = runUnitTests()
+				//if err != nil {
+				//	return err
+				//}
 			}
 
+			if nonCodeSubmissionsEnabled == "TRUE" {
+				//err = handleNonCodeSubmissions()
+				//if err != nil {
+				//	return err
+				//}
+			}
 		}
 	case "c++":
 		{
-			git.Clone(memory.NewStorage(), nil, &git.CloneOptions{URL: "https://" + githubToken + "@github.com/IUS-CS/" + repoName + ".git"})
-			if teacherUnitTestsEnabled == "TRUE" {
-				git.Clone(memory.NewStorage(), nil, &git.CloneOptions{URL: "https://" + githubToken + "@github.com/IUS-CS/AutoGraderConfig.git"})
-			}
-			if gradeDocsEnabled == "TRUE" {
-				fmt.Println("Grading c++")
-			}
+
 		}
 
 	case "python":
@@ -92,172 +100,113 @@ func main() {
 			fmt.Println(language + " is not supported. Nothing has been graded.")
 		}
 	}
-	//	ctx := context.Background()
+	return err
+}
 
-	// 	verbose := false
+func startContainer() error {
+	ctx := context.Background()
+	err := languageSwitch(ctx)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return err
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+	return err
+}
 
-	// 	containerName := *repo.Name + "-Container"
+func copyToPath() string {
+	var ret string
+	if runtime.GOOS == "windows" {
+		ret = fmt.Sprintf("%s\\AutoGrader\\%s\\%s\\%s", tempPath, courseID, assignmentName, studentUserName)
+	} else {
+		ret = fmt.Sprintf("%s/AutoGrader/%s/%s/%s", tempPath, courseID, assignmentName, studentUserName)
+	}
+	return ret
+}
 
-	// 	cli, err := client.NewClientWithOpts(client.FromEnv)
-	// 	common.CheckIfError(err)
+func copyTestsToFolder(repoName string) error {
+	cmd := exec.Command("cp", "--recursive", fmt.Sprintf("/opt/gradle/%v/src/test/java", repoName))
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return err
+}
 
-	// 	// Create and Start Container while destroying an existing container if it exists.
-	// 	common.ForceStartContainer(cli, containerName, verbose)
+func cloneRepo(githubToken string, repoName string) error {
+	cmd := exec.Command("git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", githubToken, repoName))
+	cmd.Dir = "/opt/gradle"
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return err
+}
 
-	// 	var CopyToPath string
-	// 	if runtime.GOOS == "windows" {
-	// 		CopyToPath = fmt.Sprintf("%s\\AutoGrader\\%s\\%s\\%s", common.TempPath(), assignment.CourseIDSemID, assignment.AssignmentName, common.ParseUsername(*repo.HTMLURL, assignment))
-	// 	} else {
-	// 		CopyToPath = fmt.Sprintf("%s/AutoGrader/%s/%s/%s", common.TempPath(), assignment.CourseIDSemID, assignment.AssignmentName, common.ParseUsername(*repo.HTMLURL, assignment))
-	// 	}
+func cloneConfigRepo(githubToken string) error {
+	cmd := exec.Command("git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", githubToken, config))
+	cmd.Dir = "/tmp"
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return err
+}
 
-	// 	// See if previous zip folder exists, if so delete
-	// 	if _, err := os.Stat(CopyToPath); !os.IsNotExist(err) {
-	// 		err := os.RemoveAll(CopyToPath)
-	// 		common.CheckIfError(err)
-	// 	}
+func gradeDocs(repoName string) error {
+	cmd := exec.Command("gradle", "javadoc", fmt.Sprintf("/opt/gradle/%v", repoName))
+	cmd.Dir = fmt.Sprintf("/opt/gradle/%v", repoName)
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	//tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/build/docs/javadoc", repoName))
+	//if err != nil {
+	//	return err
+	//}
 
-	// 	if assignment.AssignmentConfig.Language == "java" {
+	return err
+}
 
-	// 		common.RunDockerCommand(cli, containerName,
-	// 			[]string{"git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", common.GithubAPIKey["token"], *repo.FullName)},
-	// 			"/opt/gradle", verbose)
+//func runUnitTests() error {
+//	cmd := exec.Command("gradle", "test", fmt.Sprintf("/opt/gradle/%v", repoName)
+//	cmd.Dir = fmt.Sprintf("/opt/gradle/%v", repoName)
+//	err := cmd.Run()
+//	if err != nil {
+//		return err
+//	}
+//
+//	tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/build/test-results/test", *repo.Name))
+//	//common.CheckIfError(err)
+//
+//	if err == nil {
+//		common.HandleTarStream(tarStream, CopyToPath)
+//
+//		err = os.Rename(CopyToPath+common.PathSeparator()+"test", CopyToPath+common.PathSeparator()+"tests")
+//		common.CheckIfError(err)
+//	}
+//	return err
+//}
 
-	// 		if assignment.AssignmentConfig.TeacherUnitTests {
-	// 			//Clone Config Repo
-	// 			// Could also use //err := cli.CopyToContainer(ctx, containerName,constants.TempPath(), , types.CopyToContainerOptions{AllowOverwriteDirWithFile: false,CopyUIDGID: false} )
-	// 			// but I did not know how to use a tar reader to pass it in to the container
-	// 			// This would also prevent parallelization in the future so we when with each one downloading itself
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", common.GithubAPIKey["token"], "IUS-CS/AutoGraderConfig")},
-	// 				"/tmp", verbose)
+//func handleNonCodeSubmissions() error {
+//	tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/submission", *repo.Name))
+//
+//	if err == nil {
+//		common.HandleTarStream(tarStream, CopyToPath)
+//	}
+//}
 
-	// 			// Copy tests to folder
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"cp", "-r", ".", fmt.Sprintf("/opt/gradle/%v/src/test/java", *repo.Name)},
-	// 				fmt.Sprintf("/tmp/AutoGraderConfig/src/%v/%v/current/tests", assignment.CourseType, assignment.AssignmentName), verbose)
-	// 		}
+func main() {
+	err := startContainer()
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
 
-	// 		if assignment.AssignmentConfig.GradeDocs {
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"gradle", "javadoc"},
-	// 				fmt.Sprintf("/opt/gradle/%v", *repo.Name), verbose)
-
-	// 			tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/build/docs/javadoc", *repo.Name))
-	// 			//common.CheckIfError(err)
-
-	// 			if err == nil {
-	// 				common.HandleTarStream(tarStream, CopyToPath)
-
-	// 				err = os.Rename(CopyToPath+common.PathSeparator()+"javadoc", CopyToPath+common.PathSeparator()+"docs")
-	// 				common.CheckIfError(err)
-	// 			}
-	// 		}
-
-	// 		if assignment.AssignmentConfig.StudentTestsEnabled || assignment.AssignmentConfig.TeacherUnitTests {
-
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"gradle", "test"},
-	// 				fmt.Sprintf("/opt/gradle/%v", *repo.Name), verbose)
-
-	// 			tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/build/test-results/test", *repo.Name))
-	// 			//common.CheckIfError(err)
-
-	// 			if err == nil {
-	// 				common.HandleTarStream(tarStream, CopyToPath)
-
-	// 				err = os.Rename(CopyToPath+common.PathSeparator()+"test", CopyToPath+common.PathSeparator()+"tests")
-	// 				common.CheckIfError(err)
-	// 			}
-
-	// 		}
-
-	// 		if assignment.AssignmentConfig.NonCodeSubmissions {
-
-	// 			tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/submission", *repo.Name))
-
-	// 			if err == nil {
-	// 				common.HandleTarStream(tarStream, CopyToPath)
-	// 			}
-	// 		}
-
-	// 	} else if assignment.AssignmentConfig.Language == "c++" {
-
-	// 		common.RunDockerCommand(cli, containerName,
-	// 			[]string{"git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", common.GithubAPIKey["token"], *repo.FullName)},
-	// 			"/opt/cmake", verbose)
-
-	// 		if assignment.AssignmentConfig.TeacherUnitTests {
-	// 			//Clone Config Repo
-	// 			// Could also use //err := cli.CopyToContainer(ctx, containerName,constants.TempPath(), , types.CopyToContainerOptions{AllowOverwriteDirWithFile: false,CopyUIDGID: false} )
-	// 			// but I did not know how to use a tar reader to pass it in to the container
-	// 			// This would also prevent parallelization in the future so we when with each one downloading itself
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"git", "clone", fmt.Sprintf("https://%v@github.com/%v.git", common.GithubAPIKey["token"], "IUS-CS/AutoGraderConfig")},
-	// 				"/tmp", verbose)
-
-	// 			// Copy tests to folder
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"cp", "-r", ".", fmt.Sprintf("/opt/cmake/%v/test", *repo.Name)},
-	// 				fmt.Sprintf("/tmp/AutoGraderConfig/src/%v/%v/current/tests", assignment.CourseType, assignment.AssignmentName), verbose)
-	// 		}
-
-	// 		common.RunDockerCommand(cli, containerName,
-	// 			[]string{"cmake", "-S", fmt.Sprintf("../%v", *repo.Name), "-B", "."},
-	// 			"/opt/cmake/cmake-build-debug", verbose)
-
-	// 		common.RunDockerCommand(cli, containerName,
-	// 			[]string{"cmake", "--build", ".", "--target", "all"},
-	// 			"/opt/cmake/cmake-build-debug", verbose)
-
-	// 		// Build has to be ran and it covers doc generation too
-	// 		// Only copy over if we are grading it
-	// 		if assignment.AssignmentConfig.GradeDocs {
-	// 			tarStream, _, err := cli.CopyFromContainer(ctx, containerName, "/opt/cmake/cmake-build-debug/docs/doc_doxygen/html")
-
-	// 			if err == nil {
-	// 				common.HandleTarStream(tarStream, CopyToPath)
-
-	// 				err = os.Rename(CopyToPath+common.PathSeparator()+"html", CopyToPath+common.PathSeparator()+"docs")
-	// 				common.CheckIfError(err)
-	// 			}
-
-	// 		}
-
-	// 		if assignment.AssignmentConfig.StudentTestsEnabled || assignment.AssignmentConfig.TeacherUnitTests {
-
-	// 			common.RunDockerCommand(cli, containerName,
-	// 				[]string{"ctest", ".", "--output-junit", "./test/reports/test_output.xml"},
-	// 				"/opt/cmake/cmake-build-debug", verbose)
-
-	// 			tarStream, _, err := cli.CopyFromContainer(ctx, containerName, "/opt/cmake/cmake-build-debug/test/reports")
-
-	// 			if err == nil {
-	// 				common.HandleTarStream(tarStream, CopyToPath)
-
-	// 				err = os.Rename(CopyToPath+common.PathSeparator()+"reports", CopyToPath+common.PathSeparator()+"tests")
-	// 				common.CheckIfError(err)
-	// 			}
-	// 		}
-
-	// 		if assignment.AssignmentConfig.NonCodeSubmissions {
-
-	// 			tarStream, _, err := cli.CopyFromContainer(ctx, containerName, fmt.Sprintf("/opt/gradle/%v/submission", *repo.Name))
-	// 			if err == nil {
-	// 				common.HandleTarStream(tarStream, CopyToPath)
-	// 			}
-	// 		}
-
-	// 	} else {
-	// 		// TODO log unsupported language used and
-	// 		common.Warning("%v is not supported. Nothing has been graded.", assignment.AssignmentConfig.Language)
-	// 	}
-
-	// 	common.CheckIfError(common.StopAndRemoveContainer(cli, containerName))
-
-	//SIGTERM STUFF COPIED FROM MY LAST PROJECT
-	// fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	// sc := make(chan os.Signal, 1)
-	// signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	// <-sc
+	// done: start with environment variables
+	// done: switch statement
+	// TODO: one language at a time
+	// TODO: java>c++>duplicate
+	// TODO: sigterm handling on grader level
 }
