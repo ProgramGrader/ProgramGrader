ddc0cdbee8b76ce3601c99cd4e7433a296b0b2ff
Zac <43915749+Cliftonz@users.noreply.github.com>
2022-05-21 20:37:45 -0400

    Finished grading image

--- src/SubmissionGrader/GradeAssignments.go
+++ src/SubmissionGrader/GradeAssignments.go
@@ -11,17 +11,20 @@ func main() {
 	var wg sync.WaitGroup
 	var sharedSignal internal.GoSafeVar[bool]
 
+	fmt.Printf("Setting up os signal watcher.")
 	go internal.WatchForSignal(&sharedSignal)
 
+	fmt.Printf("Starting Grading assignment.")
 	go internal.GradeAssignment(&sharedSignal, &wg)
 
 	wg.Wait()
 
-	fmt.Println("Exiting")
+	fmt.Println("Grading Completed.")
 
 	// done: start with environment variables
 	// done: switch statement
 	// TODO: one language at a time
 	// TODO: java>c++>duplicate
-	// TODO: sigterm handling on grader level
+	// done: sigterm handling on grader level
+	// TODO: Test all functions (going forward do this before writing on when you abstract it out of a function)
 }
